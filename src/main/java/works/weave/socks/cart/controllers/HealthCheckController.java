10347c0f93d468db11e3c56e019349da4ab7ea42
Johannes Bräuer <johannes.braeuer@dynatrace.com>
2018-10-04 11:57:12 +0200

    Increasing the response time of health check for demo.

--- src/main/java/works/weave/socks/cart/controllers/HealthCheckController.java
+++ src/main/java/works/weave/socks/cart/controllers/HealthCheckController.java
@@ -37,6 +37,12 @@ public class HealthCheckController {
           database.setStatus("err");
        }
 
+       try {
+        Thread.sleep(300);
+       } catch (Throwable e) {
+        // don't do anything
+       }
+
        healthChecks.add(app);
        healthChecks.add(database);
 
