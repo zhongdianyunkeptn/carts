31e75cd041d675303276415bdf7b920fcb9d6908
Juergen Etzlstorfer <7677092+jetzlstorfer@users.noreply.github.com>
2018-11-23 10:08:39 +0100

    modify health check

--- src/main/java/works/weave/socks/cart/controllers/HealthCheckController.java
+++ src/main/java/works/weave/socks/cart/controllers/HealthCheckController.java
@@ -31,13 +31,13 @@ public class HealthCheckController {
        HealthCheck app = new HealthCheck("carts", "OK", dateNow);
        HealthCheck database = new HealthCheck("carts-db", "OK", dateNow);
 
-       /*
+       
        try {
           mongoTemplate.executeCommand("{ buildInfo: 1 }");
        } catch (Exception e) {
           database.setStatus("err");
        }
-       */
+       
 
        healthChecks.add(app);
        healthChecks.add(database);
